26442806d427d0b1631e236128e09a9a6dc33e5a
Balázs Orbán <info@balazsorban.com>
2020-10-11 03:47:55 +0200

    wip

--- index.ts
+++ index.ts
@@ -1,58 +1,92 @@
 import {
-  GetStaticPaths, GetStaticPathsResult, GetStaticProps, GetStaticPropsContext, GetStaticPropsResult
+  GetStaticPaths,
+  GetStaticPathsResult,
+  GetStaticProps,
+  GetStaticPropsContext,
+  GetStaticPropsResult,
 } from "next"
 
-export type GetStaticPropsContextWithData<D = undefined> = GetStaticPropsContext & {
+export type GetStaticPropsContextWithData<
+  D = unknown
+> = GetStaticPropsContext & {
   data: D
 }
 
-export async function defaultGetStaticProps (ctx: GetStaticPropsContextWithData) {
+export async function defaultGetStaticProps(
+  ctx: GetStaticPropsContextWithData
+) {
   return {
     props: ctx.data,
-    revalidate: 1
   }
 }
 
-export interface GetAllStaticDataParams<D, P> {
+export interface GetAllStaticDataParams<
+  D = unknown,
+  P = Record<string, unknown>
+> {
   /** Returns a list of data */
   getData(): Promise<D[]>
-  /** Return a path segment here (This will be returned from `getStaticPaths`) */
-  pathMapper(element: D): GetStaticPathsResult["paths"][0]
+  /**
+   * Which property should be used as a key
+   * (This will be returned from `getStaticPaths` as the param)
+   * */
+  key: keyof D
+  /**
+   * File name
+   * @todo Check if this can be extracted
+   */
+  name: string
   /**
    * By default,
    * the `props` return property will equal to the data,
    * and `revalidate is set to `1`.
-   * You can override this functino as you like.
+   * You can override this function as you like.
    * It is exactly like a `getStaticProps` function, only with `context.data`
    * set to the data returned from `getStaticPaths` with a matching path.
-   * See `pathMapper`
+   * See `key`
    * */
-  getStaticPropsWithData?: (context: GetStaticPropsContextWithData<D>) => Promise<GetStaticPropsResult<P>>
+  getStaticPropsWithData?: (
+    context: GetStaticPropsContextWithData<D>
+  ) => Promise<GetStaticPropsResult<P>>
   /** Fallback value for `getStaticPaths`. */
   fallback?: GetStaticPathsResult["fallback"]
 }
 
-export default function getAllStaticData<D, P>({
+export default function getAllStaticData<
+  D = unknown,
+  P = Record<string, unknown>
+>({
   getData,
-  pathMapper,
+  key,
+  name,
   getStaticPropsWithData = defaultGetStaticProps,
-  fallback = false
+  fallback = false,
 }: GetAllStaticDataParams<D, P>) {
-  const getStaticPaths: (fs: typeof import("fs")) => GetStaticPaths = (fs) =>
-    async (): Promise<GetStaticPathsResult> => {
-      const result = await getData()
-      await fs.promises.writeFile(".cache", JSON.stringify(result), "utf8")
-      return {
-        fallback,
-        paths: result.map(pathMapper)
-      }
+  const getStaticPaths: (fs: typeof import("fs")) => GetStaticPaths = (
+    fs
+  ) => async (): Promise<GetStaticPathsResult> => {
+    const result = await getData()
+    await fs.promises.writeFile(".cache", JSON.stringify(result), "utf8")
+    return {
+      fallback,
+      paths: result.map((element) => ({
+        params: { [name]: [element[key]] },
+      })),
     }
+  }
 
-  const getStaticProps: (fs: typeof import("fs")) => GetStaticProps = (fs) =>
-    async (ctx) => {
-      const result = await fs.promises.readFile(".cache", "utf8")
-      return getStaticPropsWithData({...ctx, data: JSON.parse(result).find(pathMapper)})
-    }
+  const getStaticProps: (fs: typeof import("fs")) => GetStaticProps = (
+    fs
+  ) => async (ctx) => {
+    const result = await fs.promises.readFile(".cache", "utf8")
+    const _slug = ctx.params?.[name as any] as string[]
+    const slug = _slug.flat().join("/") as any
+    const parsedResult = JSON.parse(result) as D[]
+
+    const data = parsedResult.find((element) => element[key] === slug) as D
+    console.log(slug, data)
+    return getStaticPropsWithData({ ...ctx, data })
+  }
 
   return { getStaticPaths, getStaticProps }
 }
